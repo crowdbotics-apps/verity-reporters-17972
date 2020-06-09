d2fc866508c1e670a7ceda0c77c178a1c6a2f3c7
Crowdbotics <team@crowdbotics.com>
2020-06-09 21:00:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/verity_reporters_17972/MainActivity.java
@@ -0,0 +1,15 @@
+package com.verity_reporters_17972;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "verity_reporters_17972";
+    }
+}
